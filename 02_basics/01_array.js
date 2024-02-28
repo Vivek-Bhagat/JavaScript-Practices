4261be2ec56e07a287e7bcc7b913ba733801a61c
Vivek-Bhagat <vivekbhagat1890@hotmail.com>
2024-02-28 21:06:26 +0530

    Array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,47 @@
+//Array
+
+const myArr = [1,2,3,4,5]
+// in JS array is resizable
+// JS makes the Shallow copies
+
+// console.log(myArr[0]);
+
+const myHeroes = ["Loki" , "Thor" , "Batman" , "ironman"]
+
+const newArr = new Array(1,2,3,4,5)
+
+
+
+// Array Methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+
+// myArr.unshift(9)  //pushing element at the start of the array
+// myArr.shift() //removes or shift the element
+
+// console.log(myArr.includes(9)); // to check the element is present in the array or not
+// console.log(myArr.indexOf(3)); // return the index of the element
+
+// const newArr1 = myArr.join()
+// console.log(newArr1); // array is converted into the string by using  the JOIN()
+
+
+// -----------------------------------------------
+//SPLICE , SLICE
+
+// console.log("A" , myArr);
+// const myn1 = myArr.slice(1,3) // the using the slice it doesnt change the origignal array
+// // it doesnt include the closing bound
+
+// console.log(myn1);
+// console.log("B" ,myArr);
+
+// const myn2  = myArr.splice(1,3) // by using the splice the original array go changed
+// // it includes the closing bound
+// console.log("C" , myArr);
+// console.log(myn2);
+
+// ------------------------------------------------
