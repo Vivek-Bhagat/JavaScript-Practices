2c56671aa0de0134982435c4e226283b2d46c8e3
Vivek-Bhagat <vivekbhagat1890@hotmail.com>
2024-02-29 23:48:23 +0530

    Array basic 02

---
+++ 02_basics/02_array.js
@@ -0,0 +1,16 @@
+const marvel_heroes = ["thor","ironman","superman"]
+const dc_heroes = ["superman","flash","batman"]
+
+// marvel_heroes.push(dc_heroes) // it mergers the array into the array
+// console.log(marvel_heroes);
+
+
+
+// const allHeroes =  marvel_heroes.concat(dc_heroes) // used for concatination
+// console.log(allHeroes);
+
+
+
+//new way of the concationating the array by using the dot
+// const all_new_heroes = [...marvel_heroes, ...dc_heroes]
+// console.log(all_new_heroes);
