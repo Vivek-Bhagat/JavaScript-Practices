65308a11844b94d32a0030a0608044328f3a6c5f
Vivek-Bhagat <vivekbhagat1890@hotmail.com>
2024-03-03 19:45:48 +0530

    function

--- 03_basics/01_func.js
+++ 03_basics/01_func.js
@@ -38,4 +38,35 @@ function loginUserMessage(username = "user") {
   return `${username} just Logged in`;
 }
 // console.log(loginUserMessage("User"));
-console.log(loginUserMessage()); // nothing is passed in the parameter then undefined come
+// console.log(loginUserMessage()); // nothing is passed in the parameter then undefined come
+
+function calculateCartPrice(val1, val2, ...num1) {
+  return num1;
+}
+// console.log(calculateCartPrice(200, 300, 400, 2000)); // return an array
+
+const user = {
+  username: "Sam",
+  price: 499,
+};
+
+function handleObject(anyObject) {
+  console.log(
+    ` Username is ${anyObject.username} and price is ${anyObject.price}`
+  );
+}
+
+// handleObject(user);
+handleObject({
+  username: "sam",
+  price: 499,
+});
+
+const newArray = [200, 300, 400, 500];
+
+function returnSecondValue(getArray) {
+  return getArray[1];
+}
+
+// console.log(returnSecondValue(newArray));
+console.log(returnSecondValue([100, 200, 300, 400]));
