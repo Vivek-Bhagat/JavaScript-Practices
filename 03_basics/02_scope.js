42950d068d244df252ba6794dc6f9b22aeb3dd84
Vivek-Bhagat <vivekbhagat1890@hotmail.com>
2024-03-03 20:18:36 +0530

    scope and func , hoiesting

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,13 +1,52 @@
 let c = 300;
 let a = 200;
 
+// if a function is withing the {} bracket then it is scope
 if (true) {
   let a = 10;
   const b = 20;
   // var  c = 30;   // dont use var bcz it creates a problem
-  console.log("Inner :", a);
+  //   console.log("Inner :", a);
 }
 
-console.log("Outer :", a);
+// console.log("Outer :", a);
 // console.log(b);
 // console.log(c);
+
+function one() {
+  const username = "UserOne";
+
+  function two() {
+    const website = "Youtube";
+    console.log(username);
+  }
+  //   console.log(website);  //outside the scope of the function
+  // child function can access parent function values
+
+  two();
+}
+
+// one();
+
+if (true) {
+  const username = "User";
+  if (username === "User") {
+    const website = " youtube";
+    // console.log(username + website);
+  }
+  //   console.log(website);  // outside the scope
+}
+// console.log(username);  // outside the scope
+
+// ++++++++++++++++++++ Interesting ++++++++++++++++++++++++++++++
+
+console.log(addOne(5));
+function addOne(num) {
+  return num + 1;
+}
+
+// addTwo(5)   // Hoiesting effect since function is stored in the variable
+const addTwo = function (num) {
+  return num + 2;
+};
+addTwo(5);
