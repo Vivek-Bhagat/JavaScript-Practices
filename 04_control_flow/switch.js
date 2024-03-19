71c6a5fc0ab6d48e083c852052b6aebaf95fba12
Vivek-Bhagat <vivekbhagat1890@hotmail.com>
2024-03-19 23:33:26 +0530

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,50 @@
+// switch statement
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3;
+// switch (month) {
+//   case 1:
+//     console.log("January");
+//     break;
+//   case 2:
+//     console.log("February");
+//     break;
+//   case 3:
+//     console.log("March");
+//     break;
+//   case 4:
+//     console.log("April");
+//     break;
+
+//   default:
+//     console.log("not found");
+//     break;
+// }
+
+// in case of the string format
+const month = "jan";
+switch (month) {
+  case "jan":
+    console.log("January");
+    break;
+  case "feb":
+    console.log("February");
+    break;
+  case "mar":
+    console.log("March");
+    break;
+  case "april":
+    console.log("April");
+    break;
+
+  default:
+    console.log("not found");
+    break;
+}
