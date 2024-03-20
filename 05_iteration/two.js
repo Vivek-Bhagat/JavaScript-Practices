775c69538a47a1d6df8a90c4a589b074fd34c596
Vivek-Bhagat <vivekbhagat1890@hotmail.com>
2024-03-21 01:03:31 +0530

    while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,19 @@
+// while loop
+let index = 0;
+while (index <= 10) {
+  //   console.log(`The value of the index is : ${index}`);
+  index = index + 2;
+}
+
+let myArr = ["batman", "superman", "flash"];
+let arr = 0;
+while (arr < myArr.length) {
+  //   console.log(`Values of arr : ${myArr[arr]}`);
+  arr++;
+}
+
+let score = 0;
+do {
+  console.log(`Score is : ${score}`);
+  score = score + 2;
+} while (score <= 10);
