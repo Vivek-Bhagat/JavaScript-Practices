01ee92424a2ecb3d5cd0ba4af6d3ee1981458616
Vivek-Bhagat <vivekbhagat1890@hotmail.com>
2024-03-29 19:10:28 +0530

    filter

---
+++ 05_iteration/six.js
@@ -0,0 +1,23 @@
+// FILTER  -> Returns the elements of an array that meet the condition specified in a callback function
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const newNums = myNums.filter((nums) => {
+//     return nums > 4
+// });
+// console.log(newNums);
+
+
+
+
+// const newNums = [];
+
+// myNums.forEach((num) => {
+//   if (num > 4) {
+//     newNums.push(num);
+//   }
+// });
+
+// console.log(newNums);
+
+
+
