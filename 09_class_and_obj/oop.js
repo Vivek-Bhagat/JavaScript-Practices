21af29f8d39c7ed42734a0e88c4d6cb207e4c530
Vivek-Bhagat <vivekbhagat1890@hotmail.com>
2024-05-17 20:46:21 +0530

    constructor and instances

--- 09_class_and_obj/oop.js
+++ 09_class_and_obj/oop.js
@@ -11,4 +11,24 @@ const user = {
 };
 
 console.log(user.username);
-console.log(user.getUserDetails());
+// console.log(user.getUserDetails());
+
+function User(username, loginCount, isLoggedIn) {
+  this.username = username;
+  this.loginCount = loginCount;
+  this.isLoggedIn = isLoggedIn;
+
+  this.greeting = function () {
+    console.log(`Welcome ${this.username}`);
+  };
+  return this; // implictely defined
+}
+
+// const userOne =  User("vivek", 12, true);
+// const userTwo =  User("chai", 8, false);
+// new create new instances
+const userOne = new User("vivek", 12, true);
+const userTwo = new User("chai", 8, false);
+
+console.log(userOne);
+console.log(userTwo);
